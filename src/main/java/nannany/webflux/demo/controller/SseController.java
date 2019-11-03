48460961c9fb8937b636b9d595d8a288f944731d
nannany <ymym1990ymym@gmail.com>
2019-11-03 21:46:44 +0900

    jsonデータ返すようにした

--- src/main/java/nannany/webflux/demo/controller/SseController.java
+++ src/main/java/nannany/webflux/demo/controller/SseController.java
@@ -1,11 +1,15 @@
 package nannany.webflux.demo.controller;
 
+import com.google.gson.Gson;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @RestController
@@ -17,7 +21,24 @@ public class SseController {
                 .map(sequence -> ServerSentEvent.<String>builder()
                         .id(String.valueOf(sequence))
                         .event("periodic-event")
-                        .data("SSE - " + LocalTime.now().toString())
+                        .data(getDateJsonData())
                         .build());
     }
+
+    @Data
+    @AllArgsConstructor
+    private class TimeData {
+        private int year;
+        private int month;
+        private int day;
+        private String time;
+    }
+
+    private String getDateJsonData() {
+        Gson gson = new Gson();
+        LocalDateTime ldt = LocalDateTime.now();
+        TimeData td = new TimeData(ldt.getYear(), ldt.getMonthValue(), ldt.getDayOfMonth(), ldt.toLocalTime().toString());
+        return gson.toJson(td);
+    }
 }
+
