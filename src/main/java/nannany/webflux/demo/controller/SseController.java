5df55e7ee617e262b4f5d583627ee4273efa4b84
nannany <ymym1990ymym@gmail.com>
2019-11-03 20:38:42 +0900

    Create webflux demo. Use baeldung as reference.

---
+++ src/main/java/nannany/webflux/demo/controller/SseController.java
@@ -0,0 +1,23 @@
+package nannany.webflux.demo.controller;
+
+import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.LocalTime;
+
+@RestController
+public class SseController {
+
+    @GetMapping("/stream-sse")
+    public Flux<ServerSentEvent<String>> streamEvents() {
+        return Flux.interval(Duration.ofSeconds(5))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("periodic-event")
+                        .data("SSE - " + LocalTime.now().toString())
+                        .build());
+    }
+}
