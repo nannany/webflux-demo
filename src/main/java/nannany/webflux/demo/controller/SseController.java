b081c660c9ce9a88418373996bb78f24a478a076
nannany <ymym1990ymym@gmail.com>
2019-11-17 15:37:51 +0900

    マージミスったので修正

--- src/main/java/nannany/webflux/demo/controller/SseController.java
+++ src/main/java/nannany/webflux/demo/controller/SseController.java
@@ -4,20 +4,25 @@ import com.google.gson.Gson;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.util.Random;
 
 @RestController
+@CrossOrigin
 public class SseController {
+
+    private static Gson gson = new Gson();
+    private static Random random = new Random();
 
     @GetMapping("/stream-sse")
     public Flux<ServerSentEvent<String>> streamEvents() {
-        return Flux.interval(Duration.ofSeconds(5))
+        return Flux.interval(Duration.ofSeconds(1))
                 .map(sequence -> ServerSentEvent.<String>builder()
                         .id(String.valueOf(sequence))
                         .event("periodic-event")
@@ -40,5 +45,29 @@ public class SseController {
         TimeData td = new TimeData(ldt.getYear(), ldt.getMonthValue(), ldt.getDayOfMonth(), ldt.toLocalTime().toString());
         return gson.toJson(td);
     }
+
+
+    @GetMapping("/notification/events")
+    public Flux<ServerSentEvent<String>> streamPlaceEvents() {
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> ServerSentEvent.<String>builder()
+                        .id(String.valueOf(sequence))
+                        .event("receive/roboticbase-event")
+                        .data(getPlaceJsonData())
+                        .build());
+    }
+
+    private String getPlaceJsonData() {
+        return gson.toJson(new PlaceData(random.nextInt(200), random.nextInt(200), random.nextInt(3)));
+    }
+
+    @AllArgsConstructor
+    @Data
+    private class PlaceData {
+        private Integer x;
+        private Integer y;
+        private Integer guard_mode;
+    }
+
 }
 
