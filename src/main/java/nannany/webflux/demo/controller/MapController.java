92501e0639d14ea32dd05ec91637b4de9bba60e0
nannany <ymym1990ymym@gmail.com>
2019-11-17 22:37:10 +0900

    svgも取得できるように

--- src/main/java/nannany/webflux/demo/controller/MapController.java
+++ src/main/java/nannany/webflux/demo/controller/MapController.java
@@ -4,6 +4,7 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -19,11 +20,37 @@ public class MapController {
         this.resourceLoader = resourceLoader;
     }
 
+    private enum FileTypes {
+        JPG("jpg", "jpg"), SVG("svg", "svg+xml");
+        public final String extension;
+        public final String contentType;
+
+        private FileTypes(final String extension, final String contentType) {
+            this.extension = extension;
+            this.contentType = contentType;
+        }
+    }
 
     @GetMapping("map")
-    public void downloadMap(HttpServletResponse response) throws IOException {
-        response.setHeader("Content-Type", "image/jpg");
+    public void downloadMap(@RequestParam("fileName") String fileName, HttpServletResponse response) throws IOException {
+        String[] fileNameArray = fileName.split("\\.");
+        String extension = fileNameArray[fileNameArray.length - 1];
 
-        StreamUtils.copy(resourceLoader.getResource("classpath:image/map.jpg").getInputStream(), response.getOutputStream());
+        String contentType;
+
+        switch (extension) {
+            case "jpg":
+                contentType = FileTypes.JPG.contentType;
+                break;
+            case "svg":
+                contentType = FileTypes.SVG.contentType;
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + extension);
+        }
+
+        response.setHeader("Content-Type", "image/" + contentType);
+
+        StreamUtils.copy(resourceLoader.getResource("classpath:/static/image/" + fileName).getInputStream(), response.getOutputStream());
     }
 }
